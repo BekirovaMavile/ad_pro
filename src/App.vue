89b7707c83ca3ce59201e74af31d91f49c176bae
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2022-09-20 13:22:51 +0300

    Три шага для подключения бокового меню

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app>
-    <v-navigation-drawer app></v-navigation-drawer>
+    <v-navigation-drawer app v-model="drawer"></v-navigation-drawer>
     <v-app-bar app dark color="primary">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn text><v-icon left>mdi-cake-variant</v-icon>Link One</v-btn>
@@ -17,5 +17,12 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      drawer: false,
+      sanya: 1,
+    };
+  },
+};
 </script>
