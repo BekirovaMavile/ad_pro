68626954340194d9c704aec0604ce2eada9f64c8
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2022-09-24 14:51:50 +0300

    Home Page (ссылка на главную страницу и разделение экрана на две области)

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,9 @@
 
     <v-app-bar app dark color="primary">
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>
+      <router-link to="/" tag="span" class="pointer">Home</router-link>
+    </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn v-for="link in links" :key="link.title" :to="link.url" text
