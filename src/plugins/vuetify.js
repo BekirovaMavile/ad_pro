6fccdb009019a9ec9d6b51c2e904188f4d143b96
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2022-10-20 20:43:22 +0300

    Настройка темы

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,17 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+    themes: {
+      light: {
+        primary: colors.teal.lighten2, // #4DB6AC
+        secondary: colors.teal.darken2, // #00796B
+        accent: colors.indigo.base, // #3F51B5
+      },
+    },
+  },
 });
